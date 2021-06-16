16bd3f7e1e8312c95ee90cbe33a2e726d540a678
Boaz Yoo <blazeryoo1921@gmail.com>
2021-06-16 19:29:16 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-ls -a | grep '.py' 2>&1 | tee scripts.txt
+ls -a | grep '.py' | tee scripts.txt
 read -p 'Choose one of the above options: ' SCRIPT
 
 if grep -q $SCRIPT scripts.txt; then
