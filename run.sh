48cb581650eee887c0affda0bb79fdf3f09b8fea
Boaz Yoo <blazeryoo1921@gmail.com>
2021-06-16 19:46:27 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 ls -a | grep '.py' | tee scripts.txt
 read -p 'Choose one of the above options: ' SCRIPT
 
-if [ grep -q $SCRIPT scripts.txt ]; then
+if grep -q $SCRIPT scripts.txt; then
     python $SCRIPT
 else
     echo 'Error: Choose one of the options'
